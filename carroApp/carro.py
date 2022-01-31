b3f64506663cf5a294c9edcd82e8af0285278081
Jhon Hernandez <johnher121099@gmail.com>
2022-01-31 18:10:58 -0500

    configuracion de los metodos para darle funcionalidad al carro

--- carroApp/carro.py
+++ carroApp/carro.py
@@ -1,7 +1,50 @@
-class carro():
+class carro:
     def __init__(self,request):
         self.request = request
         self.session=request.session
         carro= self.session.get('carro')
         if not carro:
             carro=self.session["carro"]={}#por si no hay un carro se crea uno con la clave y el valor
+        else:
+            self.carro=carro #trae el carro que ya esta almacenado en la session
+            
+    def agregar(self,producto):
+        if (str(producto.id)not in self.carro.keys()):#agregar un nuevo producto al carro
+            self.carro[str(producto.id)]={
+                "producto_id":producto.id,
+                "nombre":str(producto.nombrep),
+                "precio":producto.precio,
+                "cantidad":1,
+                "imagen": producto.imagen.url,
+            }
+        else:
+            for key, value in self.carro.items():
+                if key==str(producto.id):
+                    value["cantidad"]=value["cantidad"]+1
+                    break
+        self.guardar_carro()
+        
+def guardar_carro(self):
+    self.session["carro"]=self.carro
+    self.session.modified=True
+    
+def eliminacion(self,producto):
+    producto.id=str(producto.id)
+    if producto.id in self.carro:
+        del self.carro[producto.id]
+        self.guardar_carro()
+        
+def restar_unidades(self, producto):
+    for key, value in self.carro.items():
+            if key==str(producto.id):
+                value["cantidad"]=value["cantidad"]-1
+                if value["cantidad"]<1:
+                    self.eliminacion(producto)
+                break
+    self.guardar_carro()
+    
+def limpiarCarro(self):
+    self.session["carro"]={}
+    self.session.modified=True
+
+    
